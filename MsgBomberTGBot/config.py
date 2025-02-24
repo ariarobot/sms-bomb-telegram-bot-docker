04a1222566c702b2f321539fcf76c024e81f4fd7
ariarobot <yusufpremium2011@gmail.com>
2025-02-24 14:27:20 +0530

    Update config.py

--- MsgBomberTGBot/config.py
+++ MsgBomberTGBot/config.py
@@ -1,9 +1,9 @@
 import os
 class Config(object):
     # The bot-token which you can get from @Botfather
-    BOT_TOKEN = "7170629933:AAGZ6udMILU62c5ED_Q2F80AQm8pmdHGYB8"
+    BOT_TOKEN = "7170629933:AAGZ6udMILU62c5ED_Q2F80AQm8pmdHGYB8
     # There is no measure to limit who can use this bot, so add userids of users authorized to use this bot
-    AUTH_USERS = [6203428852,1981154312,-1001283809017,-1001223283623]
+    AUTH_USERS = [6203428852,1981154312,-1002156177936,-1002294375514]
     # Add numbers below who shouldn't be bombed ever
     NO_BOMB_NUMS = []
     # Add userids below of users who should have sudo authority over bot, i.e., have no bombing limits
